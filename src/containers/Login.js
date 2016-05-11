16f851715bd127c07709ee5955c1ba154e6a5027
Samantha Geitz <samanthamichele7@gmail.com>
2016-05-11 07:31:40 -0500

    Step 15: Add login and signup form

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import { reduxForm } from 'redux-form';
+
+const validate = values => {
+  const errors = {};
+
+  if (!values.email) {
+    errors.email = "Please enter an email.";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+
+  if (!values.password) {
+    errors.password = "Please enter a password.";
+  }
+
+  return errors;
+};
 
 class Login extends React.Component {
+  handleFormSubmit = (values) => {
+    console.log(values);
+  };
+
   render() {
-    return <div>Login</div>;
+    const { handleSubmit, fields: { email, password }} = this.props;
+
+    return(
+      <div className="container">
+        <div className="col-md-6 col-md-offset-3">
+          <h2 className="text-center">Log In</h2>
+
+          <form onSubmit={handleSubmit(this.handleFormSubmit)}>
+            <fieldset className={`form-group ${email.touched && email.invalid ? 'has-error' : ''}`}>
+              <label className="control-label">Email</label>
+              <input {...email} type="text" placeholder="Email" className="form-control" />
+              {email.touched ? <div className="help-block">{email.error}</div> : ''}
+            </fieldset>
+            <fieldset className={`form-group ${password.touched && password.invalid ? 'has-error' : ''}`}>
+              <label className="control-label">Password</label>
+              <input {...password} type="password" placeholder="Password" className="form-control" />
+              {password.touched ? <div className="help-block">{password.error}</div> : ''}
+            </fieldset>
+
+            <button action="submit" className="btn btn-primary">Sign In</button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Login;
+export default reduxForm({
+  form: 'login',
+  fields: ['email', 'password'],
+  validate
+})(Login);
